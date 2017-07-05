4938915e1578dad68a1b919e1e23a38b63d0306a
Genti Saliu <gentisaliu@gmail.com>
2017-07-05 15:39:47 +0200

    nginx-proxy-dotnet: rename aspnetapp to app

--- nginx-proxy-dotnet/app/Startup.cs
+++ nginx-proxy-dotnet/app/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetapp
+namespace app
 {
     public class Startup
     {
